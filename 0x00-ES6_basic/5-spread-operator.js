50c02de581a497272dd78506fdefeefdfe69e8a7
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-06-26 20:24:27 +0200

    Update 5-spread-operator.js

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,6 +1,3 @@
 export default function concatArrays(array1, array2, string) {
   return [...array1, ...array2, ...string];
 }
-
-const result = concatArrays([1, 2, 3], [4, 5, 6], "Hello");
-console.log(result); // [1, 2, 3, 4, 5, 6, 'H', 'e', 'l', 'l', 'o']
