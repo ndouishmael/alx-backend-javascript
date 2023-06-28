89030bdaf6236c558e23b10bf32697759189f24d
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-06-28 21:37:07 +0200

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUpPromise, photoPromise]).then((results) => {
-    return results.map((result) => {
-      return {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      return results.map((result) => ({
         status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason.message,
-      };
+        value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      }));
     });
-  });
 }
