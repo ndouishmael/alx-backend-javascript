dbad0aa6d0a6ec181c25ded4ddb255bdf04f8ebb
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-07-03 20:36:28 +0200

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -3,10 +3,14 @@
  * @author Ishmael Ndou <https://github.com/ndouishmael>
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
-export default function getListStudents() {
-  return [
+export function getListStudents() {
+  const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+  
+  console.log(students);
+  
+  return students;
 }
