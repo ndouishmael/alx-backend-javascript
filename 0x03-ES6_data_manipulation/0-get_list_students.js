9955266abf7b0440024120d0f9dd17802bc6a430
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-07-04 17:30:55 +0200

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,19 @@
-/**
- * Retrieves a list of students.
- * @author Ishmael Ndou <https://github.com/ndouishmael>
- * @returns {{id: Number, firstName: String, location: String}[]}
- */
-export default function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-}
+const getListStudents = () => [
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  },
+];
+
+export default getListStudents;
